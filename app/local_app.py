d6d22cafa01b37734d5c738fa26811f0e9f391ed
Pye <pyeprog@foxmail.com>
2018-11-03 07:55:43 +0800

    refactor server

--- app/local_app.py
+++ app/local_app.py
@@ -3,8 +3,25 @@ from time import sleep, time
 
 class LocalApp(object):
     def run(self):
+        self.start()
+        try:
+            self.watch(self.handler, 1)
+        except KeyboardInterrupt:
+            self.end()
+
+    def start(self):
+        print("DO some preparation")
+
+    def end(self):
+        print("DO some cleaning")
+
+    def watch(self, handler, t_interval):
         start_t = time()
         while True:
-            sleep(1)
+            sleep(t_interval)
+            handler()
             running_time = int(time() - start_t)
-            print("\rServer is running for {}s".format(running_time), end="", flush=True)
+            print("\rServer is running for {}s".format(running_time), flush=True)
+
+    def handler(self):
+        print("handle some work")
