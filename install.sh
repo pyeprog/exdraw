07653f0b5b0064756dda9a5925003748006d425f
lwl <lwl@xkool.xyz>
2020-01-23 16:47:37 +0800

    modify install script

--- install.sh
+++ install.sh
@@ -2,22 +2,16 @@
 
 pip3 install -r requirements.txt
 
-if [[ ! -d ~/bin ]]; then
-    sudo mkdir -p $HOME/bin
-    if [[ -d $HOME/bin/exdraw ]]; then
-        sudo rm -rf $HOME/bin/exdraw
+if [[ ! -d $HOME/.local/bin ]]; then
+    sudo mkdir -p "$HOME/.local/bin"
+    if [[ -d $HOME/.local/bin/exdraw ]]; then
+        sudo rm -rf "$HOME/.local/bin/exdraw"
     fi
 fi
 
-sudo cp -R $PWD/../exdraw ~/bin/exdraw
+sudo cp -R "$PWD/../exdraw $HOME/.local/bin/exdraw"
 sudo chmod 755 ~/bin/exdraw/inject
 sudo ln -s ~/bin/exdraw/inject ~/bin/inject
-
-if [ -f ~/.bashrc ]; then
-    echo "export PATH=\$PATH:$HOME/bin/" >> ~/.bashrc
-else
-    echo "export PATH=\$PATH:$HOME/bin/" > ~/.bashrc
-fi
 
 source ~/.bashrc
 echo "Installation done"
